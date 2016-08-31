63797ed2eb842f517f11fd9d4c09014263379085
Tom Taylor <tommotaylor@gmail.com>
2016-08-31 18:37:50 +0100

    App setup with redux store installed

---
+++ client/actions/actionCreators.js
@@ -0,0 +1,21 @@
+export function increment(index) {
+  type : "INCREMENT_LIKES";
+  index
+}
+
+export function addComment(postId, author, comment) {
+  return {
+    type : "ADD_COMMENT",
+    postId,
+    author,
+    comment
+  }
+}
+
+export function removeComment(postId, i) {
+  return {
+    type : "REMOVE_COMMENT",
+    postId,
+    i
+  }
+}
